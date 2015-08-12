56ab3a292660eaf892b9396e1e458fd1a6ef2c53
Myles Braithwaite <me@mylesbraithwaite.com>
2015-08-12 15:24:26 -0400

    Fixed up some stuff.

--- lib/jekyll/opml.rb
+++ lib/jekyll/opml.rb
@@ -1,7 +1,52 @@
 require "jekyll/opml/version"
 
+require 'opml'
+require 'liquid'
+
 module Jekyll
-  module Opml
-    # Your code goes here...
+  module OpmlConverter < Converter
+    safe true
+    
+    priority :low
+    
+    def matches(ext)
+      ext =~ /opml/i
+    end
+    
+    def output_ext(ext)
+      ".html"
+    end
+    
+    def convert(content)
+      content
+    end
   end
+  
+  module Filters
+    def restify(input)
+      site = @content.registers[:site]
+      converter = site.getConverterImpl(Jekyll::OpmlConverter)
+      converter.convert(input)
+    end
+  end
+  
+  class Post
+    alias :_orig_read_yaml :read_yaml
+    def read_yaml(base, name, opts={})
+      if name !~ /[.]opml$/
+        return _orig_read_yaml(base, name)
+      end
+      
+      content = File.read(File.join(base, name), merged_file_read_opts(opts))
+      self.data ||= {}
+      liquid_enabled = false;
+      
+      opml_text = Opml.new(content)
+      
+      if buffer_setting == 'liquid'
+        liquid_enabled = true
+      end
+      
+      if liquid_enabled
+        
 end
